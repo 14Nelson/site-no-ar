11bfdbc882f33374cb5d22ceaf2d9a7f79bf5206
unknown <zeferinonelson05@gmail.com>
2023-07-16 17:23:41 +0100

    nova home

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
  function Home() {
-   return <div>Home 2</div>
+   return <div>
+    <h1>Wollet Corporation</h1>
+    <p>nós somos os 3 programadores, gostamos de back todos rsrsr é engraçado de agora no fronte agente se vira kkk</p>
+    <h2>Elementos</h2>
+    
+   </div>
  }
 export default Home
