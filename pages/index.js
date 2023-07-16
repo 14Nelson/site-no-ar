77c0f278aed7890958c73364ef8cd093da7bbe91
unknown <zeferinonelson05@gmail.com>
2023-07-16 17:31:09 +0100

    nova home com elementos

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@
     <h1>Wollet Corporation</h1>
     <p>nós somos os 3 programadores, gostamos de back todos rsrsr é engraçado de agora no fronte agente se vira kkk</p>
     <h2>Elementos</h2>
-    
+    <h2>Nelson</h2>
+    <h2>André</h2>
+    <h2>Edmilson</h2>
    </div>
  }
 export default Home
